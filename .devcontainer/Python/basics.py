9180011a0f2b39f7f78e4a305988211f841f1ccf
Riya Shukla <missriyashukla@gmail.com>
2025-03-08 04:40:40 +0000

    behind the scene

---
+++ .devcontainer/Python/basics.py
@@ -0,0 +1,26 @@
+print("hello python")
+
+"""
+*******Behind the scene*******
+
+Python(compiler or software) basics.py
+
+python code => byte code => Python virtual machine(PVM)
+**Source code converts hota hai byte code me then PVM run krta hai code ko
+
+1)Compile to byteCode
+ (Low level code & Plateform independent easily 
+we can run on any system which have PVM)
+
+2)ByteCode runs faster as compared to script 
+.py => compiled python (frozen Binaries)
+
+3)_ _pycache _ _
+ Source Change & Python Version
+ -- works only for imported files
+ 
+ 4) Python Virtual machine(PVM)
+ * 
+
+
+"""
